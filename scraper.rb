3e64bda025dde032962322cc90e46629ffc4d8d7
Tony Bowden <tony@tmtm.com>
2016-07-16 07:53:44 +0100

    Allow single MORPH_TWITTER_TOKENS option
    Cutting and pasting 4 separate keys to every scraper is a little
    painful, so allow for a single 'MORPH_TWITTER_TOKENS' variable that
    decomposes into the 4 parts that we need.

--- scraper.rb
+++ scraper.rb
@@ -5,10 +5,13 @@ require 'pry'
 
 def twitter
   @twitter ||= Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV['MORPH_TWITTER_CONSUMER_KEY']
-    config.consumer_secret     = ENV['MORPH_TWITTER_CONSUMER_SECRET']
-    config.access_token        = ENV['MORPH_TWITTER_ACCESS_TOKEN']
-    config.access_token_secret = ENV['MORPH_TWITTER_ACCESS_TOKEN_SECRET']
+    if ENV.key? 'MORPH_TWITTER_TOKENS'
+      consumer_key, consumer_secret, access_token, access_secret = ENV['MORPH_TWITTER_TOKENS'].split('|')
+    end
+    config.consumer_key        = ENV['MORPH_TWITTER_CONSUMER_KEY']        || consumer_key
+    config.consumer_secret     = ENV['MORPH_TWITTER_CONSUMER_SECRET']     || consumer_secret
+    config.access_token        = ENV['MORPH_TWITTER_ACCESS_TOKEN']        || access_token
+    config.access_token_secret = ENV['MORPH_TWITTER_ACCESS_TOKEN_SECRET'] || access_secret
   end
 end
 
