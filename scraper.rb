78917bd1d54d664f56face3f28f0e2a198c48feb
Tony Bowden <tony@tmtm.com>
2016-07-19 16:43:51 +0100

    use twitter_list gem

--- scraper.rb
+++ scraper.rb
@@ -1,37 +1,6 @@
-require 'bundler/setup'
 require 'scraperwiki'
-require 'twitter'
-require 'pry'
-
-class TwitterList
-
-  attr_accessor :user, :list, :client
-
-  def initialize(user, list)
-    @user = user
-    @list = list
-    @client = Twitter::REST::Client.new do |config|
-      if ENV.key? 'MORPH_TWITTER_TOKENS'
-        consumer_key, consumer_secret, access_token, access_secret = ENV['MORPH_TWITTER_TOKENS'].split('|')
-      end
-      config.consumer_key        = ENV['MORPH_TWITTER_CONSUMER_KEY']        || consumer_key
-      config.consumer_secret     = ENV['MORPH_TWITTER_CONSUMER_SECRET']     || consumer_secret
-      config.access_token        = ENV['MORPH_TWITTER_ACCESS_TOKEN']        || access_token
-      config.access_token_secret = ENV['MORPH_TWITTER_ACCESS_TOKEN_SECRET'] || access_secret
-    end
-  end
-
-  def people
-    client.list_members(user, list).map do |person|
-      {
-        id: person.id,
-        name: person.name,
-        twitter: person.screen_name,
-        image: person.default_profile_image? ? '' : person.profile_image_url_https(:original).to_s,
-      }
-    end
-  end
-end
-
-people = TwitterList.new('lechinoise', 'politic-arg').people
-ScraperWiki.save_sqlite([:id], people)
+require 'twitter_list'
+                              
+abort "Need to set ENV['MORPH_TWITTER_TOKENS']" unless ENV.key? 'MORPH_TWITTER_TOKENS'
+twitter_list = TwitterList::Scraper.new(twitter_tokens: ENV['MORPH_TWITTER_TOKENS'])
+ScraperWiki.save_sqlite([:id], twitter_list.people('lechinoise', 'politic-arg'))
